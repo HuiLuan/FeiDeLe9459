0af0d1e3a56e579c30bb29c5709af169e31c52d2
hui luan <luanzhiyong20062@163.com>
2016-04-20 22:49:12 +0800

    .

---
+++ StudentScoreManagementMVC/StudentScoreManagementMVC/Controllers/DefaultController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using StudentScoreManagementMVC.Models;
+
+namespace StudentScoreManagementMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        AppDbContext db=new AppDbContext();
+        // GET: Default
+
+        //        public ActionResult Index()
+        //        {
+        //            return View();
+        //        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Student student)
+        {
+            
+            return RedirectToAction("Index", "StudentScore");
+        }
+
+
+
+        [AllowAnonymous]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Signup(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Default");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StudentScoreManagementMVC/StudentScoreManagementMVC/Controllers/StudentScoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentScoreManagementMVC.Controllers
+{
+    public class StudentScoreController : Controller
+    {
+        // GET: StudentScore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
